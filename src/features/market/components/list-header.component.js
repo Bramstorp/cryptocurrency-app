c6bc4ecefe09ca6630badab16a59ad362f4ab2dd
bramstorp <bramstorpmathias@gmail.com>
2021-09-17 08:52:01 +0200

    removed unused frameworks

--- src/features/market/components/list-header.component.js
+++ src/features/market/components/list-header.component.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
-import { colors } from "../../../infrastructure/theme/colors";
-
 import { TitleWrapper, Title, Divider } from "./list-header.style"
 
 export const ListHeader = () => (
